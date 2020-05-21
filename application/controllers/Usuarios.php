774e5ffec948f04d8ea4178c84de0aba2c72d5c1
itasouza <itasouza@yahoo.com.br>
2020-05-21 15:21:24 -0300

    Finalização do cadastro de usuário

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,35 +1,303 @@
 <?php
-defined('BASEPATH') OR exit('Ação não permitida');
+defined('BASEPATH') or exit('Ação não permitida');
 
 
-class Usuarios extends CI_Controller{
+class Usuarios extends CI_Controller
+{
 
-    public function index(){
+  
+
+  /*=============================================
+  =            tela de listagem           =
+  =============================================*/
+
+  public function index(){
+
+    $data = array(
+      'titulo' => 'Usuários cadastrados',
+      'sub_titulo' => 'Chegou a hora de listar os usuários cadastrados no banco de dados',
+      'usuarios'  => $this->ion_auth->users()->result(),
+
+      'styles' => array(
+        'plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+      ),
+
+      'scripts' => array(
+        'plugins/datatables.net/js/jquery.dataTables.min.js',
+        'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+        'plugins/datatables.net/js/estacionamento.js'
+      ),
+
+    );
+
+    //visualizar os dados
+    // echo '<pre>';
+    // print_r($data['usuarios']);
+    // exit();
+
+    $this->load->view('layout/header', $data);
+    $this->load->view('usuarios/index');
+    $this->load->view('layout/footer');
+  }
+   
+   /*=====  End of Section comment block  ======*/
+
+
+
+  /*=============================================
+  =            Operação de edição e criação de registro =
+  =============================================*/
+  
+
+  public function core($usuario_id = NULL)
+  {
+
+    if (!$usuario_id) {
+
+
+
+      /**
+       *
+       * cadastro de novo usuário
+       *
+       */
+      
+      $this->form_validation->set_rules('first_name', 'Nome', 'trim|required|min_length[4]|max_length[20]');
+      $this->form_validation->set_rules('last_name', 'Sobre Nome', 'trim|required|min_length[4]|max_length[20]');
+      $this->form_validation->set_rules('username', 'Usuário', 'trim|required|min_length[4]|max_length[30]|is_unique[users.username]');
+      $this->form_validation->set_rules('email', 'E-mail', 'trim|valid_email|required|min_length[5]|max_length[200]|is_unique[users.email]');
+      $this->form_validation->set_rules('password', 'Password', 'trim|min_length[8]|required');
+      $this->form_validation->set_rules('confirmacao', 'Confirmação', 'trim|matches[password]|required');
+
+      if ($this->form_validation->run()) {
+
+
+        //echo '<pre>';
+        // print_r($this->input->post());
+       // exit();
+
+
+        $username = html_escape($this->input->post('username'));  
+        $password = html_escape($this->input->post('password'));
+        $email = html_escape($this->input->post('email'));
+
+        $additional_data = array(
+          'first_name' => $this->input->post('first_name'),
+          'last_name' =>  $this->input->post('last_name'),
+          'active' => $this->input->post('active')
+        );
+
+        $group = array($this->input->post('perfil'));
+
+        //Sanitizar array
+        $additional_data = html_escape($additional_data);
+        $group = html_escape($group);
+   
+      /*
+        echo '<pre>';
+         print_r($username);
+         //print_r($password);
+        // print_r($email);
+        // print_r($additional_data);
+         //print_r($group);
+        exit();
+       */
+
+        if($this->ion_auth->register($username,$password,$email,$additional_data,$group)){
+            $this->session->set_flashdata('sucesso','Dados salvos com sucesso');
+        }else{
+            $this->session->set_flashdata('error','Não foi possivel salvar os dados');
+        }
+         
+        redirect($this->router->fetch_class()); 
+        
+
+
+        
+      }else{
+        //erro de validação
 
         $data = array(
-           'titulo' => 'Usuários cadastrados',
-           'sub_titulo' => 'Chegou a hora de listar os usuários cadastrados no banco de dados',
-           'usuarios'  => $this->ion_auth->users()->result(),
-           
-           'styles' => array(
-             'plugins/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
-           ),
-
-           'scripts' => array(
-            'plugins/datatables.net/js/jquery.dataTables.min.js',
-            'plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
-            'plugins/datatables.net/js/estacionamento.js'
-           ),
-
+          'titulo' => 'Cadastrar Usuário',
+          'sub_titulo' => 'Chegou a hora de cadastrar um novo usuário',
+          'icone_view' => 'ik ik-user',
         );
 
         //visualizar os dados
-        //echo '<pre>';
-        // print_r($data['usuarios']);
+        // echo '<pre>';
+        //  print_r($data);
         // exit();
 
         $this->load->view('layout/header', $data);
-        $this->load->view('usuarios/index');
+        $this->load->view('usuarios/core');
         $this->load->view('layout/footer');
+        
+      }
+
+    } else {
+
+      if (!$this->ion_auth->user($usuario_id)->row()) {
+
+        exit('Usuário não existe');
+
+      } else {
+
+        $perfil_atual = $this->ion_auth->get_users_groups($usuario_id)->row();
+
+
+        //editar usuário========================================================
+        $this->form_validation->set_rules('first_name', 'Nome', 'trim|required|min_length[4]|max_length[20]');
+        $this->form_validation->set_rules('last_name', 'Sobre Nome', 'trim|required|min_length[4]|max_length[20]');
+        $this->form_validation->set_rules('username', 'Usuário', 'trim|required|min_length[4]|max_length[30]|callback_username_check');
+        $this->form_validation->set_rules('email', 'E-mail', 'trim|valid_email|required|min_length[5]|max_length[200]|callback_email_check');
+        $this->form_validation->set_rules('password', 'Password', 'trim|min_length[8]');
+        $this->form_validation->set_rules('confirmacao', 'Confirmação', 'trim|matches[password]');
+
+        if ($this->form_validation->run()) {
+
+          $data = elements(
+            array(
+              'first_name',
+              'last_name',
+              'username',
+              'email',
+              'password',
+              'active',
+            ),$this->input->post()
+          );
+           
+          //verificar se a senha vai ser alterada
+          $password = $this->input->post('password'); 
+
+          //não atualiza a senha, remover do array
+          if(!$password){
+             unset($data['password']);
+          }
+
+          //Sanitizar array
+          $data = html_escape($data);
+
+          if($this->ion_auth->update($usuario_id, $data)){
+
+            //atualiza o perfil de acesso 
+            $perfil_post = $this->input->post('perfil'); 
+
+            if($perfil_atual->id != $perfil_post){
+              $this->ion_auth->remove_from_group($perfil_atual->id,$usuario_id);
+              $this->ion_auth->add_to_group($perfil_post,$usuario_id);
+            }
+
+            $this->session->set_flashdata('sucesso','Dados atualizados com sucesso');
+          }else{
+            $this->session->set_flashdata('error','Não foi possivel atualizar os dados');
+          }
+
+          redirect($this->router->fetch_class());
+
+
+        } else {
+          //erro de validação
+
+          $data = array(
+            'titulo' => 'Editar Usuário',
+            'sub_titulo' => 'Chegou a hora de editar o usuário',
+            'icone_view' => 'ik ik-user',
+            'usuario'  => $this->ion_auth->user($usuario_id)->row(),
+            'perfil_usuario' => $this->ion_auth->get_users_groups($usuario_id)->row(),
+          );
+
+          //visualizar os dados
+          // echo '<pre>';
+          //  print_r($data['perfil_usuario']);
+         //  exit();
+
+          $this->load->view('layout/header', $data);
+          $this->load->view('usuarios/core');
+          $this->load->view('layout/footer');
+        }
+
+
+        //========================================================
+      }
     }
+  }
+
+ /*=====  End of Section comment block  ======*/
+
+
+/*=============================================
+= validação usando callback para username e email   =
+=============================================*/
+
+
+  //verificar se o usuário já existe 
+  public function username_check($username){
+
+    $usuario_id = $this->input->post('usuario_id');
+    
+    if($this->core_model->get_by_id('users',array('username' => $username, 'id !=' => $usuario_id ))){
+       $this->form_validation->set_message('username_check','Esse usuário já existe');
+       return FALSE;
+    }else{
+      return TRUE;
+    }
+  }
+
+    //verificar se o email já existe 
+  public function email_check($email){
+
+      $usuario_id = $this->input->post('usuario_id');
+      
+      if($this->core_model->get_by_id('users',array('email' => $email, 'id !=' => $usuario_id ))){
+         $this->form_validation->set_message('email_check','Esse e-mail já existe');
+         return FALSE;
+      }else{
+        return TRUE;
+      }
+  }
+ 
+
+  /*=====  End of Section comment block  ======*/
+
+
+  /*=============================================
+  =           Excluir registro           =
+  =============================================*/
+  
+
+  public function del($usuario_id = NULL){
+
+    if(!$usuario_id || !$this->core_model->get_by_id('users',array('id'=>$usuario_id))){
+      
+      $this->session->set_flashdata('error','Usuário não encontrado.');
+      redirect($this->router->fetch_class());
+
+    }else{
+       
+       if(!$this->ion_auth->is_admin($usuario_id)){
+
+          $this->session->set_flashdata('error','Administrador não pode ser excluido.');
+          redirect($this->router->fetch_class());
+       }
+
+         //deletar registro
+         if($this->ion_auth->delete_user($usuario_id)){
+
+           $this->session->set_flashdata('sucesso','Registro excluido com sucesso!');
+           redirect($this->router->fetch_class());
+
+         }else{
+           $this->session->set_flashdata('error','Não foi possivel excluir o registro.');
+           redirect($this->router->fetch_class());
+         }
+
+    }
+
+  }
+  
+  
+  /*=====  End of Section comment block  ======*/
+  
+  
+
 }
--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -22,7 +22,8 @@
                         <nav class="breadcrumb-container" aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item">
-                                    <a title="Home" href="<?php echo base_url('/'); ?> "><i class="ik ik-home"></i></a>
+                                    <a data-toggle="tooltip" data-placement="bottom" 
+                                    title="Home" href="<?php echo base_url('/'); ?> "><i class="ik ik-home"></i></a>
                                 </li>
                                 <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo; ?></li>
                             </ol>
@@ -31,12 +32,39 @@
                 </div>
             </div>
 
+            <?php if ($mensagem = $this->session->flashdata('sucesso')) : ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert bg-success alert-success text-white alert-dismissible fade show" role="alert">
+                            <strong><?php echo $mensagem ?> </strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <i class="ik ik-x"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($mensagem = $this->session->flashdata('error')) : ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert bg-danger alert-danger text-white alert-dismissible fade show" role="alert">
+                            <strong><?php echo $mensagem ?> </strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <i class="ik ik-x"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
 
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <a class="btn btn-success float-right" href="">+ Novo</a>
+                            <a class="btn btn-success float-right" 
+                            data-toggle="tooltip" data-placement="right" title="Cadastrar <?php echo $this->router->fetch_class(); ?>"
+                            href="<?php echo base_url($this->router->fetch_class().'/core/'); ?>">+ Novo</a>
                         </div>
                         <div class="card-body">
                             <table class="table data-table">
@@ -52,30 +80,58 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php foreach($usuarios as $user) : ?>
-                                    <tr>
-                                        <td><?php echo $user->id; ?></td>
-                                        <td><?php echo $user->username; ?></td>
-                                        <td><?php echo $user->email; ?></td>
-                                        <td><?php echo $user->first_name; ?></td>
-                                        <td><?php echo ($this->ion_auth->is_admin($user->id) ? "Administrador" : "Atendente"); ?></td>
-                                        <td><?php echo ($user->active == 1 ? '<span class="badge badge-pill badge-success mb-1">Sim</span>' : '<span class="badge badge-pill badge-warning mb-1">Não</span>'); ?></td>
-                                        <td>
-                                            <div class="text-right">
-                                                <a data-toggle="tooltip" data-placement="bottom" title="Editar <?php echo $this->router->fetch_class(); ?>"  href="" class="btn btn-icon btn-primary"><i class="ik ik-edit-2"></i></a>
-                                                <a data-toggle="tooltip" data-placement="bottom" title="Excluir <?php echo $this->router->fetch_class(); ?>" href="" class="btn btn-icon btn-danger"><i class="ik ik-trash-2"></i></a>
+                                    <?php foreach ($usuarios as $user) : ?>
+                                        <tr>
+                                            <td><?php echo $user->id; ?></td>
+                                            <td><?php echo $user->username; ?></td>
+                                            <td><?php echo $user->email; ?></td>
+                                            <td><?php echo $user->first_name; ?></td>
+                                            <td><?php echo ($this->ion_auth->is_admin($user->id) ? "Administrador" : "Atendente"); ?></td>
+                                            <td><?php echo ($user->active == 1 ? 
+                                                '<span class="badge badge-pill badge-success mb-1"><i class="fas fa-lock-open"></i>&nbsp;Sim</span>' : '<span class="badge badge-pill badge-warning mb-1"><i class="fas fa-lock"></i>&nbsp;Não</span>'); ?>
+                                                
+                                            </td>
+
+                                            <td>
+                                                <div class="text-right">
+                                                    <a data-toggle="tooltip" data-placement="bottom" title="Editar <?php echo $this->router->fetch_class(); ?>" href="<?php echo base_url($this->router->fetch_class().'/core/'. $user->id); ?>" class="btn btn-icon btn-primary"><i class="ik ik-edit-2"></i></a>
+                                                    <button type="button"  title="Excluir <?php echo $this->router->fetch_class(); ?>" class="btn btn-icon btn-danger" data-toggle="modal" data-target="#user-<?php echo $user->id; ?>"><i class="ik ik-trash-2"></i></button> 
+                                                </div>
+                                            </td>
+                                        </tr>
+
+                           
+                                         <div class="modal fade" id="user-<?php echo $user->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalCenterLabel"><i class="fas fa-exclamation-triangle text-danger"></i>&nbsp;Tem Certeza da exclusão do registro?</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                      <p>Se deseja excluir o registro, clique em <strong>Sim, excluir</strong></p>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button  type="button" data-toggle="tooltip" data-placement="bottom" title="Cancelar" class="btn btn-secondary" data-dismiss="modal">Não, voltar</button>
+                                                        <a data-toggle="tooltip" data-placement="bottom" title="Excluir <?php echo $this->router->fetch_class(); ?>" href="<?php echo base_url($this->router->fetch_class().'/del/'. $user->id); ?>" class="btn btn-danger">Sim, Excluir</a>
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </td>
-                                    </tr>
+                                        </div>
+
+                                       
+
                                     <?php endforeach; ?>
                                 </tbody>
                             </table>
+
                         </div>
                     </div>
                 </div>
+
+
+
             </div>
-
-
 
         </div>
     </div>
